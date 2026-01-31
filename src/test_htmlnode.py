caa90061b5c04a46d1e942cf762963e9a5e7f928
bharathc10 <cb987579@gmail.com>
2026-01-31 23:12:16 +0530

    implemented leaf node

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -47,6 +47,34 @@ class TestHTMLNode(unittest.TestCase):
 
         self.assertEqual(repr(node), result)
 
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_h1(self):
+        node = LeafNode("h1", "Heading")
+        self.assertEqual(node.to_html(), "<h1>Heading</h1>")
+
+
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "Bold text")
+        self.assertEqual(node.to_html(), "<b>Bold text</b>")
+    
+    def test_leaf_to_html_with_props(self):
+        node = LeafNode(
+            "a",
+            "Click here",
+            props={
+                "href": "https://www.google.com",
+                "target": "_blank",
+            }
+        )
+
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com" target="_blank">Click here</a>'
+        )
 
 if __name__ == "__main__":
     unittest.main()
